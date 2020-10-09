40b0aecb4a8af9d80c9a940265193c5baa059877
jallend1 <jallend1@gmail.com>
2020-10-09 16:00:05 -0700

    Plays dit or dah based on length of key press, and imported Google Font

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const handleKeyPress = (e) => {
-    console.log(e.keyCode)
+const morse = document.getElementById('morse');
+const dit = new Audio('./audio/dit.mp3');
+const dah = new Audio('./audio/dah.mp3');
+let pressedAt = '';
+let keyPressed = false;
+console.dir(morse);
+
+const ditOrDah = keyPress => {
+    if(keyPress <= 150){
+        morse.innerText = 'dit.'
+        dit.play();
+
+    }
+    if(keyPress > 150){
+        morse.innerText = 'dah.'
+        dah.play();
+    }
 }
 
-window.addEventListener('keydown', handleKeyPress);
+const handleKeyDown = (e) => {
+    if(keyPressed === false){
+        keyPressed = true;
+        pressedAt = Date.now();
+    }
+}
+
+const handleKeyUp = (e) => {
+    keyPressed = false;
+    const currentTime = Date.now();
+    const keyPress = currentTime - pressedAt;
+    console.log(keyPress);
+    ditOrDah(keyPress);
+}
+
+
+window.addEventListener('keydown', handleKeyDown);
+window.addEventListener('keyup', handleKeyUp);
