04264801f1e13d5e9aab3fd22fcc65a775f85bf0
jallend1 <jallend1@gmail.com>
2020-11-02 16:43:04 -0800

    Fixes bug where it was just spitting out i's and t's relentlessly instead of translating it

--- app.js
+++ app.js
@@ -1,43 +1,5 @@
 import {codex} from './morse.js'
 
-// const codex = {
-//   a: ["dit", "dah"],
-//   b: ["dah", "dit", "dit", "dit"],
-//   c: ["dah", "dit", "dah", "dit"],
-//   d: ["dah", "dit", "dit"],
-//   e: ["dit"],
-//   f: ["dit", "dit", "dah", "dit"],
-//   g: ["dah", "dah", "dit"],
-//   h: ["dit", "dit", "dit", "dit"],
-//   i: ["dit", "dit"],
-//   j: ["dit", "dah", "dah", "dah"],
-//   k: ["dah", "dit", "dah"],
-//   l: ["dit", "dah", "dit", "dit"],
-//   m: ["dah", "dah"],
-//   n: ["dah", "dit"],
-//   o: ["dah", "dah", "dah"],
-//   p: ["dit", "dah", "dah", "dit"],
-//   q: ["dah", "dah", "dit", "dah"],
-//   r: ["dit", "dah", "dit"],
-//   s: ["dit", "dit", "dit"],
-//   t: ["dah"],
-//   u: ["dit", "dit", "dah"],
-//   v: ["dit", "dit", "dit", "dah"],
-//   w: ["dit", "dah", "dah"],
-//   x: ["dah", "dit", "dit", "dah"],
-//   y: ["dah", "dit", "dah", "dah"],
-//   z: ["dah", "dah", "dit", "dit"],
-//   1: ["dit", "dah", "dah", "dah", "dah"],
-//   2: ["dit", "dit", "dah", "dah", "dah"],
-//   3: ["dit", "dit", "dit", "dah", "dah"],
-//   4: ["dit", "dit", "dit", "dit", "dah"],
-//   5: ["dit", "dit", "dit", "dit", "dit"],
-//   6: ["dah", "dit", "dit", "dit", "dit"],
-//   7: ["dah", "dah", "dit", "dit", "dit"],
-//   8: ["dah", "dah", "dah", "dit", "dit"],
-//   9: ["dah", "dah", "dah", "dah", "dit"],
-//   0: ["dah", "dah", "dah", "dah", "dah"],
-// };
 const skillForm = document.getElementById("skillform");
 const morse = document.getElementById("morse");
 const translation = document.getElementById("translation");
@@ -119,15 +81,15 @@ const handleKeyUp = (e) => {
     keyUpTime = Date.now();
     const keyPressLength = keyUpTime - pressedAt; // Subtracts the current time from the time when the key was initially pressed
     const key = ditOrDah(keyPressLength); // Determines if dit or dah
-    newCharTimeout = window.setTimeout(extractLetter, ditLength * 10); // Sets timeout to process keypress in case it's the final one entered;
-    newWordTimeout = window.setTimeout(() => (letters += " "), ditLength * 15);
+    newCharTimeout = window.setTimeout(extractLetter, ditLength * 6); // Sets timeout to process keypress in case it's the final one entered;
+    newWordTimeout = window.setTimeout(() => (letters += " "), ditLength * 10);
     processLetter(key);
 };
 
 const isNewLetter = () => {
   if (prevPressedAt) {
     // Only runs if there is a previous character that established a comparison time
-    return keyUpTime - prevPressedAt < ditLength * 6;
+    return keyUpTime - prevPressedAt < ditLength * 3;
   } else {
     return false;
   }
